f63adce4395c90146bf2e8c415115ad26be49b1c
Chris Wright <chriswrightft@gmail.com>
2017-07-11 19:19:45 +0100

    reducing code from 3 for loops to 2

--- verboseES5.js
+++ verboseES5.js
@@ -9,66 +9,98 @@ var filterList = function(inputBoxID, listItemClass){
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
     function filterListClick(event){
-
-        var hiddenHeader = 0;
-
-        // Gather all the a links within the list items
-        var listLi = document.querySelectorAll('ul li.'+listItemClass+' a');
-
-        // on each click the value in the search box will be asssigned to a variable
-        var inputText = event.target.value.toLowerCase();
-
-        // loop through the list item a tags to change the display style
-        // when they match or fail to match the search term
-        for(var i = 0; i < listLi.length; i++){
-            // store the current list item text in a variable
-            var itemText = listLi[i].innerHTML.toLowerCase();
-
-            // Get the LI element of the current A tag
-            var itemLi = listLi[i].parentNode;
-
-            // check if the input text exists inside any of the items
-            if(itemText.indexOf(inputText) > -1){
-                // make the item visible
-                itemLi.style.display = '';
-            }else{
-                // change the display to none to remove the item from the list
-                itemLi.style.display = 'none';
-            }
-        }
-
-        // loop through the list of items and hide the collection-header if
-        // none of the list items are visible any more.
+        var inputText = event.target.value;
+        var countHeaders = 0;
         for(var i = 0; i < alphabet.length; i++){
-            // get all the items using the individual letters
-            var allLi = document.querySelectorAll('li#'+alphabet[i]);
-            var LiAmount = allLi.length;
-            var numberHidden = 0;
-
-            // loop through and increment the number of items hidden
-            // to be used to hide the header of that letter
-            for(var j = 1; j < allLi.length; j ++){
-                // if the items is not visible increment the number of hidden items
-                if(allLi[j].style.display == 'none'){
-                    numberHidden += 1;
+            var countItems = 0;
+            var items = document.querySelectorAll('ul li.'+listItemClass+'#'+alphabet[i]+' a')
+            var itemsAmount = items.length;
+            var liAmount = items.length;
+            for(var j = 0; j < items.length; j++){
+                if(items[j].innerHTML.toLowerCase().indexOf(inputText) > -1){
+                    items[j].parentNode.style.display = "";
+                }else{
+                    items[j].parentNode.style.display = "none";
+                    countItems++;
                 }
             }
 
-            // hide/display the header based on the visible items.
-            if(numberHidden == LiAmount -1){
-                document.querySelector('li#'+alphabet[i]+'.collection-header').style.display = "none";
-                hiddenHeader += 1;
+            if(countItems == itemsAmount){
+                document.getElementById(alphabet[i]).style.display = "none";
+                countHeaders++;
             }else{
-                document.querySelector('li#'+alphabet[i]+'.collection-header').style.display = "";
+                document.getElementById(alphabet[i]).style.display = "";
             }
         }
 
-        // display the default message if no headers display
-        // so users know to change their search
-        if(hiddenHeader == 26){
+        if(countHeaders == 26){
             document.getElementById('default-message').style.display = "";
         }else{
             document.getElementById('default-message').style.display = "none";
         }
     }
+
+    // function filterListClick(event){
+
+    //     var hiddenHeader = 0;
+
+    //     // Gather all the a links within the list items
+    //     var listLi = document.querySelectorAll('ul li.'+listItemClass+' a');
+
+    //     // on each click the value in the search box will be asssigned to a variable
+    //     var inputText = event.target.value.toLowerCase();
+
+    //     // loop through the list item a tags to change the display style
+    //     // when they match or fail to match the search term
+    //     for(var i = 0; i < listLi.length; i++){
+    //         // store the current list item text in a variable
+    //         var itemText = listLi[i].innerHTML.toLowerCase();
+
+    //         // Get the LI element of the current A tag
+    //         var itemLi = listLi[i].parentNode;
+
+    //         // check if the input text exists inside any of the items
+    //         if(itemText.indexOf(inputText) > -1){
+    //             // make the item visible
+    //             itemLi.style.display = '';
+    //         }else{
+    //             // change the display to none to remove the item from the list
+    //             itemLi.style.display = 'none';
+    //         }
+    //     }
+
+    //     // loop through the list of items and hide the collection-header if
+    //     // none of the list items are visible any more.
+    //     for(var i = 0; i < alphabet.length; i++){
+    //         // get all the items using the individual letters
+    //         var allLi = document.querySelectorAll('li#'+alphabet[i]);
+    //         var LiAmount = allLi.length;
+    //         var numberHidden = 0;
+
+    //         // loop through and increment the number of items hidden
+    //         // to be used to hide the header of that letter
+    //         for(var j = 1; j < allLi.length; j ++){
+    //             // if the items is not visible increment the number of hidden items
+    //             if(allLi[j].style.display == 'none'){
+    //                 numberHidden += 1;
+    //             }
+    //         }
+
+    //         // hide/display the header based on the visible items.
+    //         if(numberHidden == LiAmount -1){
+    //             document.querySelector('li#'+alphabet[i]+'.collection-header').style.display = "none";
+    //             hiddenHeader += 1;
+    //         }else{
+    //             document.querySelector('li#'+alphabet[i]+'.collection-header').style.display = "";
+    //         }
+    //     }
+
+    //     // display the default message if no headers display
+    //     // so users know to change their search
+    //     if(hiddenHeader == 26){
+    //         document.getElementById('default-message').style.display = "";
+    //     }else{
+    //         document.getElementById('default-message').style.display = "none";
+    //     }
+    // }
 };
